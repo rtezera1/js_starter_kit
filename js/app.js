a132bb74bbb935a8d65694ae3bb707666ee67df1
Robel <robeltadesse01@gmail.com>
2014-03-03 09:00:31 -0500

    final

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 alert('Hey find me to know where to put the javascript');
+
+selected_number=Math.random() ;
+var name= prompt("what is your name?")
+var guessed_number=prompt('Guess a number!!')
+if (guessed_number===selected_number) {
+  alert("you have won bro");
+
+} else {
+  alert('try again sir');
+}
